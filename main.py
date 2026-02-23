398268265b3485ccb49887348f83150c53bc947a
shui <ewan.schwaller@epita.fr>
2026-02-23 02:53:41 -0500

    add watershed and quickshift

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ import argparse
 from pathlib import Path
 from preprocessing import traitement
 from segmentation_basic import segmentation_basic
-# from segmentation_watershed import watershed_segmentation
+from segmentation_watershed import watershed_segmentation
 # from segmentation_advanced import segmentation_advanced
+from quickshift import segmentation_quickshift
 from features import process_segmentation_results
 
 def main():
@@ -31,14 +32,14 @@ def main():
     seg_watershed_path = "results/seg_watershed/"
     os.makedirs(seg_watershed_path, exist_ok=True)
     print("--- Segmentation Watershed ---")
-    # objects_in_images = watershed_segmentation(preprocessing_path)
-    # process_segmentation_results(objects_in_images, preprocessing_path, seg_watershed_path + "features.csv", seg_watershed_path)
+    objects_in_images = watershed_segmentation(preprocessing_path)
+    process_segmentation_results(objects_in_images, preprocessing_path, seg_watershed_path + "features.csv", seg_watershed_path)
 
     seg_advanced_path = "results/seg_advanced/"
     os.makedirs(seg_advanced_path, exist_ok=True)
     print("--- Segmentation Avancée ---")
-    # objects_in_images = segmentation_advanced(preprocessing_path)
-    # process_segmentation_results(objects_in_images, preprocessing_path, seg_advanced_path + "features.csv", seg_advanced_path)
+    objects_in_images = segmentation_quickshift(preprocessing_path)
+    process_segmentation_results(objects_in_images, preprocessing_path, seg_advanced_path + "features.csv", seg_advanced_path)
 
 if __name__ == "__main__":
     main()
